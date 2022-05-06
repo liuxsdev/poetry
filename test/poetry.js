093f4f1e286b01f47d1562f4d8045e0c19d0ddd0
liuxsdev <1028330144@qq.com>
2022-05-06 23:40:26 +0800

    add test

---
+++ test/poetry.js
@@ -0,0 +1,36 @@
+import { expect } from "chai";
+
+import { get_random_uid, parse_uid, get_poetry_data_by_uid } from "../dist/index.js";
+
+describe("test get_random_uid", () => {
+    it("should return a random number as string", () => {
+        expect(get_random_uid()).to.be.a("string");
+    });
+});
+
+describe("test parse_uid", () => {
+    it("should return a Object", () => {
+        expect(parse_uid("32277")).to.be.an("object");
+    });
+    it("check object property", () => {
+        let uid = get_random_uid();
+        let paresd = parse_uid(uid);
+        expect(paresd).to.have.property("uid").to.be.a("string");
+        expect(paresd).to.have.property("type").to.be.a("number");
+        expect(paresd).to.have.property("id").to.be.a("number");
+        expect(paresd)
+            .to.have.property("type_string")
+            .to.be.oneOf(["tang", "song", "ci"]);
+        expect(paresd)
+            .to.have.property("collection")
+            .to.be.oneOf(["全唐诗", "全宋诗", "全宋词"]);
+        expect(paresd).to.have.property("poetry_time").to.oneOf(["唐", "宋"]);
+    });
+});
+
+describe("get a random poetry", async () => {
+    let data = await get_poetry_data_by_uid(get_random_uid());
+    // await expect(data).to.have.property("extra");
+    // expect(data).to.have.property("poetry_data");
+    console.log(data);
+});
